48deec6648d680cceb6e0a9bd36f15d88522b8fa
TakahashiNguyen <TakahashiNguyen@users.noreply.github.com>
2025-01-28 17:16:09 +0000

    deploy: 8d2b4a0d7d45017a917f9e5bee7012d345cda331

---
+++ build/utils/combine-styles.js
@@ -0,0 +1,28 @@
+import { flatten, unflatten } from 'flat';
+import merge from 'lodash/merge.js';
+import * as theme from "../theme.js";
+/**
+ * Applies new styles to the default theme
+ *
+ * ### Example
+ *
+ * ```jsx
+ * import { combineStyles } from '@adminjs/design-system`
+ *
+ * const myTheme = combineStyles({
+ *   colors: {
+ *     primary100: '#000'
+ *   }
+ * })
+ * ```
+ *
+ * @param {ThemeOverride} overrides
+ * @memberof module:@adminjs/design-system
+ *
+ * @return  {Theme}
+ */
+const combineStyles = (...overrides) => {
+  const merged = merge(flatten(theme), ...overrides.map((o = {}) => flatten(o)));
+  return unflatten(merged);
+};
+export { combineStyles };
